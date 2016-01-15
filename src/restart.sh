4460e251d7207d798c3020de6c7c022685015229
Tao Ren <tao@swarmnyc.com>
2016-01-15 23:40:48 +0000

    Move shell script to the root.

--- src/restart.sh
+++
@@ -1,7 +0,0 @@
-sudo service  kurento-media-server-6.0  stop
-sudo rm /var/log/kurento-media-server/media-server_201*
-make
-sudo service  kurento-media-server-6.0  start
-
-touch ../kurento-test-nodejs/server.js 
-
