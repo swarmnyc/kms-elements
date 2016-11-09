9b35f9f7eec513d62866c189ec514169dd4b9fbc
Tao Ren <tao@swarmnyc.com>
2016-11-09 22:27:17 +0000

    Update docker file and script.

--- docker/generate.sh
+++ docker/generate.sh
@@ -12,9 +12,12 @@ make
 echo
 echo Package all .so files to stylecompositor.tar.gz:
 /bin/sleep 3
+rm ../stylecompositor.tar.gz
 tar czvf ../stylecompositor.tar.gz src/server/lib* src/gst-plugins/lib* gst-plugins-bad/libgstcompositor.so
 
 echo
 echo copy nodejs module to kurento-test-nodejs...
 /bin/sleep 2
+rm ../stylecompositor_js.tar.gz
+tar czvf ../stylecompositor_js.tar.gz ./js/*
 cp -r ./js/* ../../kurento-test-nodejs/node_modules/kurento-client/node_modules/kurento-client-elements/
